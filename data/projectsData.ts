18c542f77fabefa3c50836af04bca21c8c6b036f
Benjamin Jørgensen <me@benmi.me>
2024-06-05 21:19:41 +0900

    some formating

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,7 +3,7 @@ const projectsData = [
     title: 'A blog',
     description: ` Simple blog made out of a tailwind nextjs template`,
     imgSrc: '/static/images/google.png',
-    href: '/blog/2023/12/12/why-I-chose-to-build-out-my-own-blog',
+    href: '/blog/why-I-chose-to-build-out-my-own-blog',
   },
 ]
 
